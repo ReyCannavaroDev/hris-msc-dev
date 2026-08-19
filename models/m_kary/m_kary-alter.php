8f4d28cb1c9557b373d28bce306d5fedc93a4add
Rey Cannavaro <reyjunoalcannavaro@gmail.com>
2026-08-19 10:33:22 +0700

    feat: add updated_num and updated_year columns to m_kary table

--- models/m_kary/m_kary-alter.php
+++ models/m_kary/m_kary-alter.php
@@ -10,16 +10,8 @@ class mkary extends Migration
     public function up()
     {
         Schema::table($this->tableName, function (Blueprint $table) {
-            //$table->string('_existColumnName_')->change();
-            //$table->string('_columnName_');
-            // $table->dropColumn(['ig','x','facebook','linkedin','email']);
-            // $table->boolean('can_outscope')->default(false)->nullable();
-            //$table->bigInteger('tipe_karyawan_id')->comment('{"src":"m_general.id"}')->nullable();
-            // $table->string('ig',100)->nullable();
-            // $table->string('x',100)->nullable();
-            // $table->string('facebook',100)->nullable();
-            // $table->string('linkedin',100)->nullable();
-            // $table->string('email',100)->nullable();
+            $table->integer('updated_num')->default(0)->nullable();
+            $table->integer('updated_year')->nullable();
         });
     }
 }
